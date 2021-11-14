fe6ab0e7a591d376000752704ede2ccd5321185b
bernabe <bernabe.torres@agilecontent.com>
2021-11-14 13:05:50 +0100

    Add Lambda Predicate examples.

---
+++ src/com/bernabe/java8/Lambda/ExamplePredicate.java
@@ -0,0 +1,21 @@
+package com.bernabe.java8.Lambda;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+public class ExamplePredicate {
+
+  public static void main(String[] args) {
+
+    Predicate<Integer> test = num -> num > 10;
+    System.out.println("test.test(9) = " + test.test(9));
+    System.out.println("test.test(11) = " + test.test(11));
+
+    BiPredicate<String, Integer> test2 = (a,b) -> a.trim().length() == b;
+    System.out.println("test2.test(\"Ringo\"), 3 = " + test2.test("Ringo", 3));
+    System.out.println("test2.test(\"Ringo\"), 5 = " + test2.test("Ringo", 5));
+
+
+  }
+
+}
